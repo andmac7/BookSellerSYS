f2f19ef4c3a74d3c0f63c71d2113005ce31ba5fd
bengt69 <andreas.macsotay@gmail.com>
2015-01-27 13:40:08 +0100

    Hello

---
+++ ConsoleApplication2/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Seller
+    {
+        public String fName { get; set; }
+        public String lName { get; set; }
+        public String phoneNbr { get; set; }
+        public String mail { get; set; }
+        public String city { get; set; }
+        public String password { get; set; }
+    }
+    public class BookAd
+    {
+        public String title { get; set; }
+        public String isbn { get; set; }
+        public String author { get; set; }
+        public String date { get; set; }
+        public String price { get; set; }
+        public String addText { get; set; }
+    }
+
+    public class Course
+    {
+        public String cCode { get; set; }
+        public String cName { get; set; }
+    }
+
+    public class Institution
+    {
+        public String instName { get; set; }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seller seller = new Seller();
+            seller.fName = "Bengt";
+            seller.lName = "Bengtsson";
+            System.Console.WriteLine("{0} {1}", seller.fName, seller.lName);
+            System.Console.Read();
+        }
+    }
+}
