e9ac594dd58016807b2a8d55f08746a5c760ddc4
bengt69 <andreas.macsotay@gmail.com>
2015-01-27 14:36:02 +0100

    Databasklass

--- ConsoleApplication2/Class2.cs
+++ ConsoleApplication2/Class2.cs
@@ -10,6 +10,29 @@ namespace ConsoleApplication2
     {
         private string sqlString;
         private string strCon;
-        private string lol;
+        System.Data.SqlClient.SqlDataAdapter da;
+        public String SqlString 
+        { 
+            set { sqlString = value; } 
+        }
+        public String StrCon 
+        { 
+            set { strCon = value; } 
+        }
+        public System.Data.DataSet GetConnection
+        {
+            get { return MyDataSet(); }
+        }
+
+        private System.Data.DataSet MyDataSet()
+        {
+            System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(strCon);
+            con.Open();
+            da = new System.Data.SqlClient.SqlDataAdapter(sqlString, con);
+            System.Data.DataSet datSet = new System.Data.DataSet();
+            da.Fill(datSet, "Table_Data");
+            con.Close();
+            return datSet;
+        }
     }
 }
